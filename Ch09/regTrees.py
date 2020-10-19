7eed2fe47f19632e13d42744c110b50680cfe792
DuanShengqi <Shengqi_QD@163.com>
2020-10-19 10:06:43 +0800

    第九章树回归修改至可以正常运行

--- Ch09/regTrees.py
+++ Ch09/regTrees.py
@@ -10,13 +10,13 @@ def loadDataSet(fileName):      #general function to parse tab -delimited floats
     fr = open(fileName)
     for line in fr.readlines():
         curLine = line.strip().split('\t')
-        fltLine = map(float,curLine) #map all elements to float()
+        fltLine = list(map(float,curLine)) #map all elements to float()
         dataMat.append(fltLine)
     return dataMat
 
 def binSplitDataSet(dataSet, feature, value):
-    mat0 = dataSet[nonzero(dataSet[:,feature] > value)[0],:][0]
-    mat1 = dataSet[nonzero(dataSet[:,feature] <= value)[0],:][0]
+    mat0 = dataSet[nonzero(dataSet[:,feature] > value)[0],:]
+    mat1 = dataSet[nonzero(dataSet[:,feature] <= value)[0],:]
     return mat0,mat1
 
 def regLeaf(dataSet):#returns the value used for each leaf
@@ -55,7 +55,7 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
     S = errType(dataSet)
     bestS = inf; bestIndex = 0; bestValue = 0
     for featIndex in range(n-1):
-        for splitVal in set(dataSet[:,featIndex]):
+        for splitVal in set(dataSet[:,featIndex].A[0]): #由于哈希的问题，在set()时需要进行一些形式的转换
             mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
             if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): continue
             newS = errType(mat0) + errType(mat1)
@@ -134,3 +134,17 @@ def createForeCast(tree, testData, modelEval=regTreeEval):
     for i in range(m):
         yHat[i,0] = treeForeCast(tree, mat(testData[i]), modelEval)
     return yHat
+
+
+
+# dataset = loadDataSet(r'C:\Users\Administrator\Desktop\机器学习\SourceCode\machinelearninginaction\Ch09\ex0.txt')
+# mydat = mat(dataset)
+# y = createTree(mydat)
+# print(y)
+
+
+
+
+
+
+
--- Ch09/treeExplore.py
+++ Ch09/treeExplore.py
@@ -1,6 +1,6 @@
 from numpy import *
 
-from Tkinter import *
+from tkinter import *
 import regTrees
 
 import matplotlib
@@ -20,21 +20,21 @@ def reDraw(tolS,tolN):
     else:
         myTree=regTrees.createTree(reDraw.rawDat, ops=(tolS,tolN))
         yHat = regTrees.createForeCast(myTree, reDraw.testDat)
-    reDraw.a.scatter(reDraw.rawDat[:,0], reDraw.rawDat[:,1], s=5) #use scatter for data set
+    reDraw.a.scatter(reDraw.rawDat[:,0].A, reDraw.rawDat[:,1].A, s=5) #use scatter for data set
     reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0) #use plot for yHat
-    reDraw.canvas.show()
+    reDraw.canvas.draw()
     
 def getInputs():
     try: tolN = int(tolNentry.get())
     except: 
         tolN = 10 
-        print "enter Integer for tolN"
+        print ("enter Integer for tolN")
         tolNentry.delete(0, END)
         tolNentry.insert(0,'10')
     try: tolS = float(tolSentry.get())
     except: 
         tolS = 1.0 
-        print "enter Float for tolS"
+        print ("enter Float for tolS")
         tolSentry.delete(0, END)
         tolSentry.insert(0,'1.0')
     return tolN,tolS
@@ -47,7 +47,7 @@ root=Tk()
 
 reDraw.f = Figure(figsize=(5,4), dpi=100) #create canvas
 reDraw.canvas = FigureCanvasTkAgg(reDraw.f, master=root)
-reDraw.canvas.show()
+reDraw.canvas.draw()
 reDraw.canvas.get_tk_widget().grid(row=0, columnspan=3)
 
 Label(root, text="tolN").grid(row=1, column=0)
@@ -63,7 +63,7 @@ chkBtnVar = IntVar()
 chkBtn = Checkbutton(root, text="Model Tree", variable = chkBtnVar)
 chkBtn.grid(row=3, column=0, columnspan=2)
 
-reDraw.rawDat = mat(regTrees.loadDataSet('sine.txt'))
+reDraw.rawDat = mat(regTrees.loadDataSet(r'C:\Users\Administrator\Desktop\机器学习\SourceCode\machinelearninginaction\Ch09\sine.txt'))
 reDraw.testDat = arange(min(reDraw.rawDat[:,0]),max(reDraw.rawDat[:,0]),0.01)
 reDraw(1.0, 10)
                
